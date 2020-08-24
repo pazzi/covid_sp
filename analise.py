09735f2b6b88fe24b761821f20b8dfcd2b022823
Carlos Benjamim Pazzianotto <carlos.pazzianotto@gmail.com>
2020-08-23 21:18:52 -0300

    Alterações em analise.py e analise_sp.ipynb

--- analise.py
+++ analise.py
@@ -1,34 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
+
+def dados(cidade,df1,x):
+        print(x)
+        print(cidade)
+        f.write(str(x) + ' - ')
+        f.write(cidade + '\n')
+        filt=df1['nome_munic'] == cidade
+        dia=df1[filt]['datahora'].tolist()
+        print("Dia:{}".format(dia[0]))
+        f.write("Dia:{}".format(dia[0]) + '\n')
+        pop=df1[filt]['pop'].tolist()
+        print("Populacao:" + str(pop[0]))
+        f.write("Populacao:" + str(pop[0]) + '\n')
+        total = df1[filt]['obitos'].tolist()
+        print("Obitos:{}".format(total[0]))
+        f.write("Obitos:" + str(total[0]) + '\n')
+        print("Indice (obitos):" + str(total[0]/pop[0]))
+        f.write("Indice (obitos):" + str(total[0]/pop[0]) + '\n')
+        total=df1[filt]['casos'].tolist()
+        print("Total de casos:" + str(total[0]))
+        f.write("Total de casos:" + str(total[0]) + '\n')
+        print("Indice (casos):" + str(total[0]/pop[0]))
+        f.write("Indice (casos):" + str(total[0]/pop[0]) + '\n')
+        hoje=df1[filt]['casos_novos'].tolist()
+        print("Casos hoje:" + str(hoje[0]))
+        f.write("Casos hoje:" + str(hoje[0])+ '\n')
+        print("***************************************************")
+        f.write("***************************************************\n")
+
 df=pd.read_csv("https://raw.githubusercontent.com/seade-R/dados-covid-sp/master/data/dados_covid_sp.csv", sep=";", encoding="utf-8")
 f=open("saida.txt", "a")
-def dados(cidade):
-	print(cidade)
-	f.write(cidade + '\n')
-	filt=df['nome_munic'] == cidade
-	pop=df[filt]['pop'].tolist()
-	print("Populacao:" + str(pop[0]))
-	f.write("Populacao:" + str(pop[0]) + '\n')
-	total = df[filt]['obitos'].tolist()
-	print("Obitos:" + str(total[len(total)-1]))
-	f.write("Obitos:" + str(total[len(total)-1]) + '\n')
-	print("Indice (obitos):" + str(total[len(total)-1]/pop[0]))
-	f.write("Indice (obitos):" + str(total[len(total)-1]/pop[0]) + '\n')
-	total=df[filt]['casos'].tolist()
-	print("Total de casos:" + str(total[len(total)-1]))
-	f.write("Total de casos:" + str(total[len(total)-1]) + '\n')
-	print("Indice (casos):" + str(total[len(total)-1]/pop[0]))
-	f.write("Indice (casos):" + str(total[len(total)-1]/pop[0]) + '\n')
-	hoje=df[filt]['casos_novos'].tolist()
-	print("Casos hoje:" + str(hoje[len(total)-1]))
-	f.write("Casos hoje:" + str(hoje[len(total)-1])+ '\n')
-	print("***************************************************")
-	f.write("***************************************************\n")
-cidades=df['nome_munic'].tolist()
+df.sort_values(by=['casos'], inplace=True, ascending=False)
+filt = (df['datahora'] == df['datahora'].max())
+df1 = df[filt]
+cidades=df1['nome_munic'].tolist()
 cidades_un = list(dict.fromkeys(cidades))
-cidades_un.sort()
-
+#cidades_un.sort()
+x=0
 for cidade in cidades_un:
-	dados(cidade)
+	if cidade != "Ignorado":
+		x+=1
+		dados(cidade,df,x)
 f.close()
